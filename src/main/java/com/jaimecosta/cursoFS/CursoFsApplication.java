c9261d96e337aaab29371160314ab361290a76bc
Jaime Costa <jaimecostaramos@gmail.com>
2024-09-25 17:10:04 +0100

    Criando operação de instanciação

--- src/main/java/com/jaimecosta/cursoFS/CursoFsApplication.java
+++ src/main/java/com/jaimecosta/cursoFS/CursoFsApplication.java
@@ -1,13 +1,33 @@
 package com.jaimecosta.cursoFS;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoFsApplication {
+import com.jaimecosta.cursoFS.domain.Categoria;
+import com.jaimecosta.cursoFS.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoFsApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoFsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
