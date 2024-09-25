026f4cf43c56d32d691a005621abe573010ac428
Jaime Costa <jaimecostaramos@gmail.com>
2024-09-25 15:10:36 +0100

    Projeto Criado

---
+++ src/main/java/com/jaimecosta/cursoFS/CursoFsApplication.java
@@ -0,0 +1,13 @@
+package com.jaimecosta.cursoFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaimecosta/cursoFS/CursoFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaimecosta.cursoFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
