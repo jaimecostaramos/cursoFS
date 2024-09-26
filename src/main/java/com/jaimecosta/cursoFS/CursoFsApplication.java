44221b9497c33798128a3c99955a2bbff7aeeade
Jaime Costa <jaimecostaramos@gmail.com>
2024-09-26 17:39:27 +0100

    Estado e Cidade

--- src/main/java/com/jaimecosta/cursoFS/CursoFsApplication.java
+++ src/main/java/com/jaimecosta/cursoFS/CursoFsApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jaimecosta.cursoFS.domain.Categoria;
+import com.jaimecosta.cursoFS.domain.Cidade;
+import com.jaimecosta.cursoFS.domain.Estado;
 import com.jaimecosta.cursoFS.domain.Produto;
 import com.jaimecosta.cursoFS.repositories.CategoriaRepository;
+import com.jaimecosta.cursoFS.repositories.CidadeRepository;
+import com.jaimecosta.cursoFS.repositories.EstadoRepository;
 import com.jaimecosta.cursoFS.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,7 +23,10 @@ public class CursoFsApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoFsApplication.class, args);
@@ -42,9 +49,23 @@ public class CursoFsApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
+			
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Uberlândia", est1);
+		Cidade c2 = new Cidade(null,"São Paulo", est2);
+		Cidade c3 = new Cidade(null,"Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 
 }
